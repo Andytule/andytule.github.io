d45eca94e544934aab5c9900ef9f41873a353aa5
Andytule <Andytule321@gmail.com>
2023-12-28 06:27:11 -0500

    Completed Home Page

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,10 +1,26 @@
 import React from "react";
 import "./Home.scss";
+import { TypeAnimation } from "react-type-animation";
 
 const Home: React.FC = () => {
   return (
     <div className="home">
-      <h2>Hello! I'm Andy</h2>
+      <h2 className="header">
+        <TypeAnimation
+          sequence={[
+            "Hi! I'm Andy.",
+            5000,
+            "こんにちは！私はアンディです。",
+            5000,
+            "Have a wonderful day.",
+            5000,
+            "素敵な一日を",
+            5000,
+          ]}
+          style={{ fontSize: "2em" }}
+          repeat={Infinity}
+        />
+      </h2>
     </div>
   );
 };
