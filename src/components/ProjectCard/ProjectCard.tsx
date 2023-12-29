8c9664fe206fe03cf3394b07ac9308bf7a496107
Andytule <Andytule321@gmail.com>
2023-12-29 04:44:51 -0500

    Updated Projects section styling

--- src/components/ProjectCard/ProjectCard.tsx
+++ src/components/ProjectCard/ProjectCard.tsx
@@ -10,18 +10,18 @@ interface ProjectCardProps {
 const ProjectCard: React.FC<ProjectCardProps> = ({ project }) => {
   return (
     <div className="project">
-      <h3>{project.title}</h3>
-      <p>{project.description}</p>
-      {project.imageName && (
-        <img
-          src={images[project.imageName]}
-          alt={project.title}
-          style={{ width: "10%" }}
-        />
-      )}
-      <p>Demo: {project.demoLink}</p>
-      <p>Github: {project.githubLink}</p>
-      <p>Technologies: {project.technologies.join(", ")}</p>
+      <img
+        className="project-image"
+        src={images[project.imageName]}
+        alt={project.title}
+      />
+      <div>
+        <h3 className="project-title">{project.title}</h3>
+        <p className="project-description">{project.description}</p>
+        <p>Demo: {project.demoLink}</p>
+        <p>Github: {project.githubLink}</p>
+        <p>Technologies: {project.technologies.join(", ")}</p>
+      </div>
     </div>
   );
 };
