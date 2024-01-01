bb9e1929df8e1ac65339423c62957689d8ba7ec2
Andytule <Andytule321@gmail.com>
2024-01-01 04:53:12 -0500

    Updated Projects page

--- src/pages/Projects/Projects.tsx
+++ src/pages/Projects/Projects.tsx
@@ -3,6 +3,7 @@ import "./Projects.scss";
 import ProjectCard from "../../components/ProjectCard/ProjectCard";
 import Masonry, { ResponsiveMasonry } from "react-responsive-masonry";
 import projectsData from "../../data/projectsData";
+import sleepingCat from "../../assets/images/sleeping-cat.svg";
 
 export interface Project {
   title: string;
@@ -19,6 +20,11 @@ const Projects: React.FC = () => {
       <div className="projects-container">
         <ResponsiveMasonry columnsCountBreakPoints={{ 1000: 1, 1001: 2 }}>
           <Masonry gutter="3rem">
+            <div className="sleeping-cat-container">
+              <div className="sleeping-cat">
+                <img src={sleepingCat} alt="Sleeping Cat" />
+              </div>
+            </div>
             {projectsData.map((project: Project, index: number) => (
               <ProjectCard project={project} />
             ))}
