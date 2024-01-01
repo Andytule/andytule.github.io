5bb274bcee6fcc83492f2c9a0c3b3bb9e90bd60c
Andytule <Andytule321@gmail.com>
2023-12-31 22:48:00 -0500

    Adjusted sidebar styles

--- src/components/Sidebar/SideBar.tsx
+++ src/components/Sidebar/SideBar.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import "./SideBar.scss";
 import { slide as BurgerMenu } from "react-burger-menu";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faHome,
@@ -13,6 +13,7 @@ import {
 
 const SideBar: React.FC = () => {
   const [isOpen, setIsOpen] = useState(false);
+  const location = useLocation();
 
   const handleStateChange = (state: { isOpen: boolean }) => {
     setIsOpen(state.isOpen);
@@ -24,23 +25,43 @@ const SideBar: React.FC = () => {
 
   return (
     <BurgerMenu isOpen={isOpen} onStateChange={handleStateChange}>
-      <Link to="/" onClick={closeMenu}>
+      <Link
+        to="/"
+        onClick={closeMenu}
+        className={location.pathname === "/" ? "active-link" : ""}
+      >
         <FontAwesomeIcon icon={faHome} />
         Home
       </Link>
-      <Link to="/projects" onClick={closeMenu}>
+      <Link
+        to="/projects"
+        onClick={closeMenu}
+        className={location.pathname === "/projects" ? "active-link" : ""}
+      >
         <FontAwesomeIcon icon={faLaptopCode} />
         Projects
       </Link>
-      <Link to="/experiences" onClick={closeMenu}>
+      <Link
+        to="/experiences"
+        onClick={closeMenu}
+        className={location.pathname === "/experiences" ? "active-link" : ""}
+      >
         <FontAwesomeIcon icon={faBriefcase} />
         Work Experience
       </Link>
-      <Link to="/resume" onClick={closeMenu}>
+      <Link
+        to="/resume"
+        onClick={closeMenu}
+        className={location.pathname === "/resume" ? "active-link" : ""}
+      >
         <FontAwesomeIcon icon={faFile} />
         Resume
       </Link>
-      <Link to="/contact" onClick={closeMenu}>
+      <Link
+        to="/contact"
+        onClick={closeMenu}
+        className={location.pathname === "/contact" ? "active-link" : ""}
+      >
         <FontAwesomeIcon icon={faAddressCard} />
         Contact
       </Link>
