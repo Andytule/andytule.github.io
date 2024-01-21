3549b00036b58a8588edc819cdd16d8665340819
Andytule <Andytule321@gmail.com>
2024-01-21 01:40:32 -0500

    Updated packages and resume screen

--- src/pages/Resume/Resume.tsx
+++ src/pages/Resume/Resume.tsx
@@ -1,9 +1,12 @@
 import React from "react";
+import "./Resume.scss";
 
 const Resume: React.FC = () => {
   return (
     <div className="resume">
-      <h2>Resume</h2>
+      <div className="resume-container">
+        <h2 className="section-title">Resume</h2>
+      </div>
     </div>
   );
 };
